968d8b0bb03845312e025ced4e90cd4cd5edd560
jsavas <savasjonathan@gmail.com>
2020-04-03 19:58:00 -0400

    added home page and started encrypt page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,226 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(EnpictionApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class EnpictionApp extends StatelessWidget {
+  static const String _title = 'Enpiction';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    final _buttonWidth = 200.0;
+    final _buttonHeight = 75.0;
+
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: MaterialApp(
+      title: _title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        buttonTheme: ButtonThemeData(
+          minWidth: _buttonWidth,
+          height: _buttonHeight,
+        ),
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          button: TextStyle(fontSize: 25.0)
+        ),
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage()
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatelessWidget {
+  HomePage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Enpiction')),
+      body: Center(
+        child: IntrinsicWidth(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Select action:',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(height: Theme.of(context).buttonTheme.height),
+              RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EncryptPage())
+                    );
+                  },
+                  child: Text(
+                    'Encrypt',
+                    style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
+                  ),
+                ),
+              SizedBox(height: Theme.of(context).buttonTheme.height  / 1.5),
+              RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Decrypt',
+                    style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class EncryptPageState extends State<EncryptPage> {
+  final _encryptEntries = <Tuple2<String, String>>[];
+  final _left = true;
+  final _right = false;
+  final _textController = TextEditingController();
+  File _image;
 
-  void _incrementCounter() {
+  bool _isNextEnabled() {
+    return _image != null && _textController.text.length > 0;
+  }
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _image = image;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Encrypt'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            _imageWidget(),
+            _inputTextField(),
+            _bottomCornerButton(_left, 'Cancel', () => Navigator.pop(context)),
+            _bottomCornerButton(_right, 'Next', _isNextEnabled() ? _saveEncryptionEntry : () => {}),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+    );
+  }
+
+  void _saveEncryptionEntry() {
+    setState(() {
+      _encryptEntries.add(Tuple2<String, String>(_image.path, _textController.text));
+      _textController.clear();
+      _image = null;
+    });
+  }
+
+  Widget _imageWidget() {
+    final _imageContainer = _image == null
+        ? Column(
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+              Container(
+                child: FloatingActionButton.extended(
+                  onPressed: getImage,
+                  label: Text('Choose an image'),
+                  backgroundColor: Colors.pink,
+                  icon: Icon(Icons.image),
+                )
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+            ]
+          )
+        : Column(
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                  image: AssetImage(_image.path)
+                  )
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+            ]
+          );
+
+    return Align(
+      alignment: Alignment(0.0, -0.7),
+      child: _imageContainer
+    );
+  }
+
+  Widget _bottomCornerButton(bool left, String text, Function buttonAction) {
+    final sign = left ? -1 : 1;
+    final _buttonTextTheme = Theme.of(context).textTheme.button;
+    final _localButtonText = TextStyle(fontSize: _buttonTextTheme.fontSize * 2 / 3);
+    final _buttonData = Theme.of(context).buttonTheme;
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
+
+    return Align(
+      alignment: Alignment(sign * 0.9, (_height - 0.1 * _width) / _height),
+      child: ButtonTheme(
+        minWidth: _buttonData.minWidth * 2 / 3,
+        height: _buttonData.height * 2 / 3,
+        child: RaisedButton(
+          onPressed: buttonAction,
+          child: Text(text, style: _localButtonText),
+          textColor: Colors.white,
+          elevation: 5,
+        )
+      )
+    );
+  }
+
+  Widget _inputTextField() {
+    return Align(
+      alignment: Alignment(0.0, 0.0),
+      child: Container(
+        width: 350.0,
+        child: TextField(
+          controller: _textController,
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+          maxLength: 256,
+          autocorrect: false,
+          textAlign: TextAlign.center,
+          minLines: 1,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+            ),
+            hintText: 'Enter text to hide in this picture'
+          ),
+        )
+      )
     );
   }
 }
+
+class EncryptPage extends StatefulWidget {
+  @override
+  EncryptPageState createState() => EncryptPageState();
+}
+
