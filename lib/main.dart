998ceacbd19aa7334e0863f1dab78f255e457a0f
jsavas <savasjonathan@gmail.com>
2020-04-08 17:20:27 -0400

    cleaned up Encrypt route

--- lib/main.dart
+++ lib/main.dart
@@ -24,23 +24,31 @@ class EnpictionApp extends StatelessWidget {
         }
       },
       child: MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        buttonTheme: ButtonThemeData(
-          minWidth: _buttonWidth,
-          height: _buttonHeight,
+        title: _title,
+        theme: ThemeData(
+          buttonTheme: ButtonThemeData(
+            minWidth: _buttonWidth,
+            height: _buttonHeight,
+          ),
+          textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+            button: TextStyle(fontSize: 25.0)
+          ),
+          primarySwatch: Colors.blue,
         ),
-        textTheme: TextTheme(
-          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-          button: TextStyle(fontSize: 25.0)
-        ),
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage()
+        home: HomePage(),
+        routes: {
+          '/encryptChoose': (context) => EncryptChoosePage(),
+          '/encryptSubmitKey': (context) => EncryptSubmitKeyPage(),
+        },
       ),
     );
+  }
+
+  static void returnHome(BuildContext context) {
+    Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
   }
 }
 
@@ -64,9 +72,9 @@ class HomePage extends StatelessWidget {
               SizedBox(height: Theme.of(context).buttonTheme.height),
               RaisedButton(
                   onPressed: () {
-                    Navigator.push(
+                    Navigator.pushNamed(
                       context,
-                      MaterialPageRoute(builder: (context) => EncryptChoosePage())
+                      EncryptChoosePage.routeName
                     );
                   },
                   child: Text(
@@ -120,10 +128,15 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
   }
 
   Future getImage() async {
+    String text = _textController.text;
+
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       _image = image;
+      _textFieldFocus.unfocus();
+      if (text.length > 0)
+        _textController.text = text;
     });
   }
   
@@ -135,40 +148,67 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
     }
   }
 
+  void goBackFromQuestion() {
+    setState(() {
+      _encryptEntries.removeLast();
+      _isEncryptButtonClicked = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text('Encrypt'),
-      ),
-      body: _isEncryptButtonClicked ? _encryptQuestionWidget() : _mainEncryptPageWidget()
-    );
+    if (_isEncryptButtonClicked) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Encrypt'),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return BackButton(
+                onPressed: goBackFromQuestion,
+              );
+            },
+          ),
+        ),
+        body: _encryptQuestionWidget()
+      );
+    } else {
+      return Scaffold(
+          resizeToAvoidBottomInset: true,
+          appBar: AppBar(
+            title: Text('Encrypt'),
+          ),
+          body: _mainEncryptPageWidget()
+      );
+    }
   }
 
   Widget _mainEncryptPageWidget() {
     var _widgetStack = <Widget>[
-      _imageWidget(),
-      _inputTextField(_isFocusOnTextField),
-      _encryptFloatingButton(),
-      _bottomCornerButton(_left, 'Cancel', () => Navigator.pop(context)),
+      ListView(
+        padding: const EdgeInsets.all(30),
+        children: <Widget>[
+          _imageWidget(),
+          SizedBox(height: Theme.of(context).buttonTheme.height / 3),
+          _inputTextField(_isFocusOnTextField),
+          SizedBox(height: Theme.of(context).buttonTheme.height / 3),
+          _encryptFloatingButton(),
+        ]
+      ),
     ];
 
-    if (_encryptEntries.length < _maxEntries - 1) {
-      _widgetStack.add(
-          _bottomCornerButton(_right, 'Next', _isNextEnabled() ? _nextButtonAction : () => {})
-      );
+    if (!_isFocusOnTextField) {
+      _widgetStack.add(_bottomCornerButton(_left, 'Cancel', () => EnpictionApp.returnHome(context)));
+
+      if (_encryptEntries.length < _maxEntries - 1) {
+        _widgetStack.add(
+            _bottomCornerButton(
+                _right, 'Next', _isNextEnabled() ? _nextButtonAction : () => {})
+        );
+      }
     }
 
-    return ! _isFocusOnTextField
-        ? Stack(
-            children: _widgetStack
-          )
-        : Stack(
-            children: <Widget>[
-              _inputTextField(_isFocusOnTextField),
-            ]
-        );
+    return Stack(children: _widgetStack);
   }
 
   void _saveEncryptionEntry() {
@@ -202,7 +242,6 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
           )
         : Column(
             children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
               Container(
                 width: MediaQuery.of(context).size.width * 0.5,
                 height: MediaQuery.of(context).size.height * 0.3,
@@ -212,14 +251,10 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
                   )
                 ),
               ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
             ]
           );
 
-    return Align(
-      alignment: Alignment(0.0, -0.7),
-      child: _imageContainer
-    );
+    return _imageContainer;
   }
 
   Widget _bottomCornerButton(bool left, String text, Function buttonAction) {
@@ -246,28 +281,30 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
   }
 
   Widget _encryptFloatingButton() {
-    return Align(
-      alignment: Alignment(0.0, 0.5),
-      child: FloatingActionButton.extended(
-          onPressed: _isNextEnabled()
-              ? _encryptButtonAction
-              : () => {},
+    void _encryptButtonAction() {
+      // Need this in case that button is pressed while keyboard has focus,
+      // otherwise it would not detect the change in the text field
+      setState((){});
+
+      if (_isNextEnabled()) {
+        setState(() {
+          _saveEncryptionEntry();
+          _isEncryptButtonClicked = true;
+        });
+      }
+    }
+
+    return FloatingActionButton.extended(
+          onPressed: _encryptButtonAction,
           label: Text('Encrypt'),
           backgroundColor: Colors.cyan,
           icon: Icon(Icons.lock),
           heroTag: null,
-      ),
     );
   }
 
-  void _encryptButtonAction() {
-    setState(() {
-      _isEncryptButtonClicked = true;
-    });
-  }
-
   Widget _encryptQuestionWidget() {
-    final _len = _encryptEntries.length + 1;
+    final _len = _encryptEntries.length;
     return Scaffold(
       body: Center(
         child: IntrinsicWidth(
@@ -282,8 +319,11 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
               SizedBox(height: Theme.of(context).buttonTheme.height),
               RaisedButton(
                 onPressed: () {
-                  _saveEncryptionEntry();
-                  Navigator.of(context).pop();
+                  Navigator.pushNamed(
+                    context,
+                    EncryptSubmitKeyPage.routeName,
+                    arguments: _encryptEntries
+                  );
                 },
                 child: Text(
                   'Encrypt!',
@@ -292,11 +332,7 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
               ),
               SizedBox(height: Theme.of(context).buttonTheme.height  / 1.5),
               RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _isEncryptButtonClicked = false;
-                  });
-                },
+                onPressed: goBackFromQuestion,
                 child: Text(
                   'Go back',
                   style: Theme.of(context).textTheme.button.copyWith(color: Colors.white),
@@ -310,9 +346,7 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
   }
 
   Widget _inputTextField(bool hasFocus) {
-    return Align(
-      alignment: hasFocus ? Alignment(0.0, -0.7): Alignment(0.0, 0.0),
-      child: Container(
+    return Container(
         width: 350.0,
         child: TextField(
           autofocus: hasFocus, // not working as expected
@@ -320,6 +354,7 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
           controller: _textController,
           inputFormatters: [WhitelistingTextInputFormatter(RegExp("[\x00-\x7F]"))],
           keyboardType: TextInputType.multiline,
+          textInputAction: TextInputAction.done,
           maxLines: null,
           maxLength: 128,
           autocorrect: false,
@@ -331,14 +366,92 @@ class EncryptChoosePageState extends State<EncryptChoosePage> {
             hintText: 'Enter text to hide in this picture'
           ),
         )
-      )
     );
   }
 }
 
 class EncryptChoosePage extends StatefulWidget {
+  static const routeName = '/encryptChoose';
+
   @override
   EncryptChoosePageState createState() => EncryptChoosePageState();
+}
+
+class EncryptSubmitKeyPageState extends State<EncryptSubmitKeyPage> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Tuple2<String, String>> encryptEntries = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: Text('Encrypt')
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(50),
+        children: <Widget>[
+            _inputKeyField(),
+            SizedBox(height: Theme.of(context).buttonTheme.height / 1.5),
+            _encryptFloatingButton(context),
+          ],
+        ),
+    );
+  }
+
+  Widget _inputKeyField() {
+    return Container(
+        width: 350.0,
+        child: TextField(
+          autofocus: true,
+          controller: _textController,
+          inputFormatters: [WhitelistingTextInputFormatter(RegExp("[\x00-\x7F]"))],
+          keyboardType: TextInputType.multiline,
+          textInputAction: TextInputAction.done,
+          maxLines: null,
+          maxLength: 16,
+          autocorrect: false,
+          textAlign: TextAlign.center,
+          minLines: 1,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+              ),
+              hintText: 'Enter an encryption key:'
+          ),
+        )
+    );
+  }
+
+  Widget _encryptFloatingButton(BuildContext context) {
+    void buttonAction() {
+      setState((){});
+
+      if (_textController.text.length > 0)
+        EnpictionApp.returnHome(context);
+    }
+
+    return FloatingActionButton.extended(
+        onPressed: buttonAction,
+        label: Text('Encrypt'),
+        backgroundColor: Colors.cyan,
+        icon: Icon(Icons.lock),
+        heroTag: null,
+      );
+  }
+}
+
+class EncryptSubmitKeyPage extends StatefulWidget {
+  static const routeName = '/encryptSubmitKey';
+
+  @override
+  EncryptSubmitKeyPageState createState() => EncryptSubmitKeyPageState();
 }
 
 
