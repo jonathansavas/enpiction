8ffb57de02c70c068ffe1d115235b866cd5ace71
jsavas <savasjonathan@gmail.com>
2020-04-29 23:07:54 -0400

    added JpegHider class that hides hardcoded 'message' in a Jpeg

--- android/app/src/main/java/com/github/jsavas/enpiction/MainActivity.java
+++ android/app/src/main/java/com/github/jsavas/enpiction/MainActivity.java
@@ -1,64 +1,13 @@
 package com.github.jsavas.enpiction;
 
 import androidx.annotation.NonNull;
-import com.github.jsavas.enpiction.steganography.StegFiles;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-import java.util.List;
-import java.util.Map;
-
 public class MainActivity extends FlutterActivity {
-  private static final String ENCODE_CHANNEL = "com.github.jsavas/encode";
-  private static final String DECODE_CHANNEL = "com.github.jsavas/decode";
-
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), ENCODE_CHANNEL)
-      .setMethodCallHandler(
-        (call, result) -> {
-          if (call.method.equals("encode")) {
-
-            boolean successfulEncoding = StegFiles.encode(
-              (Map<String, String>) call.argument("pathsToMessages"),
-              call.argument("encryptionKey"));
-
-            System.runFinalization();
-            System.gc();
-
-            if (successfulEncoding) {
-              result.success(true);
-            } else {
-              result.error("UNAVAILABLE", "Failed to encode messages", false);
-            }
-          } else {
-            result.notImplemented();
-          }
-        }
-      );
-
-    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), DECODE_CHANNEL)
-      .setMethodCallHandler(
-        (call, result) -> {
-          if (call.method.equals("decodeAndValidate")) {
-
-            List<String> messages = StegFiles.decodeAndValidate(
-              (List<String>) call.argument("filePaths"),
-              call.argument("encryptionKey"));
-
-            System.runFinalization();
-            System.gc();
-
-            result.success(messages);
-          } else {
-            result.notImplemented();
-          }
-        }
-      );
-
     GeneratedPluginRegistrant.registerWith(flutterEngine);
   }
-
 }
--- test/steg_test.dart
+++ test/steg_test.dart
@@ -6,8 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:enpiction/steganography/crypto.dart';
+import 'package:enpiction/steganography/jpeg_hider.dart';
 import 'package:enpiction/steganography/steg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image/image.dart';
@@ -71,5 +73,46 @@ void main() {
 
     invalid.add(invalidPrefix + messages[3]);
     expect(messageFinder.validateMessageSet(invalid), equals(false));
+  });
+
+  test('Test hide in jpeg', () {
+    String imgPath = 'test/test_jpg.jpg';
+    List<int> imgBytes = File(imgPath).readAsBytesSync();
+    for (int i = 0; i < 50; i++) {
+
+      List<int> withHidden = JpegHider().hideMessage(decodeJpg(imgBytes));
+
+      JpegData jpegA = JpegData()
+        ..read(withHidden);
+      Uint8List msgBytes = Uint8List.fromList('message'.codeUnits);
+
+      List<int> m = [];
+      int b = 0;
+      int bitNum = 0;
+
+      int idx = 2;
+
+      int blockNum = 0;
+      int elemNum = 0;
+      while (m.length < msgBytes.length) {
+        for (int i = 0; i < 3; i++) {
+          int bit = jpegA.frame.components.values
+              .elementAt(i)
+              .blocks
+              .elementAt(blockNum)
+              .elementAt(elemNum)
+              .elementAt(idx) & 1;
+          b = (b << 1) | bit;
+          bitNum++;
+          if (bitNum == 8) {
+            m.add(b);
+            b = 0;
+            bitNum = 0;
+          }
+        }
+        elemNum++;
+      }
+      expect(String.fromCharCodes(m), equals(String.fromCharCodes(msgBytes)));
+    }
   });
 }
